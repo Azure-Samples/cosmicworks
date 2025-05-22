a733a969c48b6d52c2a4de8e053a317c44773c1b
Sidney Andrews <sidandrews@microsoft.com>
2025-05-22 17:07:40 -0400

    Add placeholder test

---
+++ src/tests/integration/tool/PlaceholderTests.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+namespace Microsoft.Samples.Cosmos.NoSQL.CosmicWorks.Tool.Tests.Integration;
+
+public class PlaceholderTests
+{
+    [Fact]
+    public void TrueTest()
+    {
+        Assert.True(true);
+    }
+}
